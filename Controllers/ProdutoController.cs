e019e00ad32334e201e7f4d413d387d1cce44651
Bruno Santos <bcs.2707@gmail.com>
2022-02-14 23:51:26 -0300

    Adicionar arquivos de projeto.

---
+++ Controllers/ProdutoController.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+
+namespace LojaWeb.Controllers
+{
+    public class ProdutoController : ControllerBase
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        private IActionResult View()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
